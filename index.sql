41426b2a5a0cc61122caec2101cdccd2ccdadea9
Raja <craja26@gmail.com>
2019-10-22 11:49:50 +0200

    Update and rename Index to index.sql

--- index.sql
+++ index.sql
@@ -1,8 +1,8 @@
 # Track create index query progress.
-  Add below statement before "CREATE INDEX.." statement
-    SET STATISTICS PROFILE ON
+/*  Add below statement before "CREATE INDEX.." statement */
+SET STATISTICS PROFILE ON
     
-  Run the following statement in new query window to track the create index progress
+#  Run the following statement in new query window to track the create index progress
     SELECT session_id, request_id, physical_operator_name, node_id, 
            thread_id, row_count, estimate_row_count
     FROM sys.dm_exec_query_profiles
