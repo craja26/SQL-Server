016ad37981118f6743aa2d750d4d68b3ffb7b7ec
Raja <craja26@gmail.com>
2020-06-18 17:29:42 +0200

    Update troubleshoot.sql

--- replication/troubleshoot.sql
+++ replication/troubleshoot.sql
@@ -5,3 +5,9 @@ go
 
 select * from dbo.MSrepl_errors
 where error_code in ('2601','2627','25098') 
+
+#2 browse undistributed commands
+USE distribution;
+EXEC sp_browsereplcmds '0x00002D34003F4164005100000000', '0x00002D34003F4164005100000000'
+GO
+
